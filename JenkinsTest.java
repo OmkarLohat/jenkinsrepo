c4289a1c0780e609827de441e148bf72d2c392ed
Omkar Lohat <omkarlohat1408@gmail.com>
2024-06-27 15:27:22 +0530

    Commited

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -2,5 +2,6 @@ public class JenkinsTest{
   public static void main(String args[])
   {
     System.out.println("I am a java file in Git repo");
+    System.out.println("Changes done later");
   }
 }
